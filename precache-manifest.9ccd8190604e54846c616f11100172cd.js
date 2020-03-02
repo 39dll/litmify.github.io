a155674f07aee9c03452b56daf62197b2ad62ed5
Fi <litmify@gmail.com>
2020-03-02 16:54:58 +0900

    Updates

---
+++ precache-manifest.9ccd8190604e54846c616f11100172cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7bbde1d33375872f38e3eb425d8b40",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f29aff0e6e8c602216c",
+    "url": "/static/css/main.655319ae.chunk.css"
+  },
+  {
+    "revision": "83bdae3753dd6c6b5b91",
+    "url": "/static/js/2.57499e5e.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.57499e5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f29aff0e6e8c602216c",
+    "url": "/static/js/main.49792474.chunk.js"
+  },
+  {
+    "revision": "a812c191e5a58b06aeb9",
+    "url": "/static/js/runtime-main.6cc51021.js"
+  }
+]);
